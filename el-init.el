1a96eb6a09e84f922a4a42096e0d937e7dc1a4d7
HKey <s06139@gmail.com>
2015-01-06 21:34:07 +0900

    Add header and footer comments

--- el-init.el
+++ el-init.el
@@ -1,6 +1,34 @@
-;;;; Utilities
+;;; el-init.el --- A loader inspired by init-loader -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Hiroki YAMAKAWA
+
+;; Author: Hiroki YAMAKAWA <s06139@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((cl-lib "0.5"))
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
 
 (require 'cl-lib)
+
+;;;; Utilities
 
 (defmacro el-init::aif (test then &rest else)
   (declare (indent 2))
@@ -290,3 +318,4 @@
 
 
 (provide 'el-init)
+;;; el-init.el ends here

a14995f3273afcd33ebdbfc25a680c53193c99d4
HKey <s06139@gmail.com>
2015-01-07 11:52:21 +0900

    Add tests about el-init:record

--- test/el-init-test.el
+++ test/el-init-test.el
@@ -3,4 +3,13 @@
 (require 'ert)
 (require 'el-init)
 
+;;;; Record
+
+(ert-deftest el-init-test:record ()
+  (let ((el-init:record nil))
+    (el-init:add-record 'el-init-test 'foo "foo")
+
+    (should (string= (el-init:get-record 'el-init-test 'foo) "foo"))
+    (should (eq (el-init:get-record 'el-init-test 'bar) nil))))
+
 ;;; el-init-test.el ends here

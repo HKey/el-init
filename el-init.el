b9b6d7b26652dcc49c75519016ddd3c1005b3043
HKey <s06139@gmail.com>
2015-01-19 13:32:58 +0900

    Add docstrings

--- el-init.el
+++ el-init.el
@@ -43,6 +43,7 @@
 
 ;;;###autoload
 (defun el-init:provide ()
+  "Call `provide' with the file name as a feature name."
   (provide
    (el-init::file-name-to-symbol
     (or load-file-name
@@ -53,22 +54,32 @@
 ;;;; Record
 
 (defvar el-init:record nil
-  "alist (feature . plist (prop val))")
+  "A record which is used by `require' wrapper functions.
+This is a list of alists.
+The alist consists of a feature name and a plist.
+The plist consists of wrapper names and values.
+
+Example:
+  ((init-foo wrapper-foo value-foo-1 wrapper-bar value-bar-1 ...)
+   (init-bar wrapper-foo value-foo-2 wrapper-bar value-bar-2 ...) ...)")
 
 (defun el-init:get-feature-record (feature)
+  "Get a record plist of FEATURE from `el-init:record'."
   (cdr (assoc feature el-init:record)))
 
 (defun el-init:get-record (feature property)
+  "Get a record value of PROPERTY of FEATURE from `el-init:record'."
   (plist-get (el-init:get-feature-record feature) property))
 
 (defun el-init:add-record (feature property value)
+  "Add a record value of PROPERTY of FEATURE to `el-init:record'."
   (aif (assoc feature el-init:record)
       (setf (cdr (assoc feature el-init:record))
             (plist-put (cdr it) property value))
     (push (cons feature (list property value)) el-init:record)))
 
 (with-no-warnings
-  ;; Since Emacs 24.3, `defsetf' has been obsolete.
+  ;; Since emacs 24.3, `defsetf' has been obsolete.
   (if (fboundp 'gv-define-setter)
       (gv-define-setter el-init:get-record (value feature property)
         `(el-init:add-record ,feature ,property ,value))
@@ -79,38 +90,56 @@
 
 ;;;; Alert
 
-(defvar el-init:alert-buffer-name "*el-init alerts*")
+(defvar el-init:alert-buffer-name "*el-init alerts*"
+  "A name of a buffer which `el-init:alert' outputs to.")
 
-(defvar el-init:alert-function #'el-init:default-alert)
+(defvar el-init:alert-function #'el-init:default-alert
+  "A function which is called by `el-init:alert'.
+It has a parameter which is a string to display like `el-init:default-alert'.")
 
-(defvar el-init:alert-enable-p t)
+(defvar el-init:alert-enable-p t
+  "When its value is non-nil, `el-init:alert' displays messages.")
 
 (defun el-init:alert (message)
+  "Display MESSAGE as an alert.
+When `el-init:alert-enable-p' is nil, this function does nothing.
+This function just calls `el-init:alert-function'."
   (when el-init:alert-enable-p
     (funcall el-init:alert-function message)))
 
 (defun el-init:default-alert (message)
+  "Default alert function of `el-init:alert'."
   (display-warning 'el-init message nil el-init:alert-buffer-name))
 
 
 
 ;;;; Require Wrapper
 
-(defvar el-init:wrappers '(el-init:require/record-error))
-
-(define-obsolete-variable-alias
-  'el-init:load-function-list
-  'el-init:wrappers
-  "0.1.0")
-
-(defvar el-init::require-wrappers nil)
+(defvar el-init::require-wrappers nil
+  "[internal] A `require' wrapper function list which is used by `el-init:next'.")
 
 (defun el-init:next (feature &optional filename noerror)
+  "Call the next `require' wrapper function."
   (let ((fn (car el-init::require-wrappers))
         (el-init::require-wrappers (cdr el-init::require-wrappers)))
     (funcall fn feature filename noerror)))
 
 (defmacro el-init:define-require (name &rest body)
+  "This is an obsolete macro.
+
+Migration guide:
+- Change `el-init:define-require' to `defun'
+- Add parameter list: (feature &optional filename noerror)
+- Add feature, filename and noerror to calling `el-init:next'
+
+Example:
+  ;; version 0.0.9
+  (el-init:define-require my-require/ignore-errors
+    (ignore-errors (el-init:next)))
+
+  ;; version 0.1.0
+  (defun my-require/ignore-errors (feature &optional filename noerror)
+    (ignore-errors (el-init:next feature filename noerror)))"
   (declare (indent 1))
   (warn "`el-init:define-require' is obsolete.")
   (let ((next (cl-gensym)))
@@ -125,6 +154,13 @@
 
 ;; benchmark
 (defun el-init:require/benchmark (feature &optional filename noerror)
+  "A `require' wrapper function to benchmark loading FEATURE with `benchmark-run'.
+Its score is a list as a return value of `benchmark-run' and recorded to
+`el-init:record'.
+
+Example:
+  (el-init:get-record 'init-foo 'el-init:require/benchmark)
+  ;; => (3.6666e-05 0 0.0)"
   (let ((result (benchmark-run (el-init:next feature filename noerror))))
     (unless (el-init:get-record feature 'el-init:require/benchmark)
       (el-init:add-record feature 'el-init:require/benchmark result))))
@@ -132,6 +168,12 @@
 
 ;; record error
 (defun el-init:require/record-error (feature &optional filename noerror)
+  "A `require' wrapper function to record errors which occur while `el-init:load' is loading.
+The record value is an error value and recorded to `el-init:record'.
+
+Example:
+  (el-init:get-record 'init-foo 'el-init:require/record-error)
+  ;; => (error \"Required feature `init-foo' was not provided\")"
   (condition-case e
       (el-init:next feature filename noerror)
     (error (el-init:add-record feature 'el-init:require/record-error e)
@@ -140,12 +182,23 @@
 
 ;; ignore error
 (defun el-init:require/ignore-errors (feature &optional filename noerror)
+  "A `require' wrapper function to ignore errors which occur while `el-init:load' is loading.
+This wrapper records no values."
   (ignore-errors (el-init:next feature filename noerror)))
 
 
 ;; eval-after-load
 (defun el-init:require/record-eval-after-load-error
     (feature &optional filename noerror)
+  "A `require' wrapper function to record errors which occur in `eval-after-load' form.
+The record value is a list of plists and recorded to `el-init:record'.
+The plist consists of `:file' property and `:error' property.
+`:file' property is the value of file argument of `eval-after-load' and
+`:error' property is an error value.
+
+Example:
+  (el-init:get-record 'init-foo 'el-init:require/record-eval-after-load-error)
+  ;; => ((:file lib-bar :error (error \"Error\")) ...)"
   (let* ((original (symbol-function 'eval-after-load))
          (e (cl-gensym))
          (fn (lambda (file form)
@@ -165,25 +218,80 @@
 
 ;; switching by the system (from init-loader)
 
-(defvar el-init:meadow-regexp       "^init-meadow-")
-(defvar el-init:carbon-emacs-regexp "^init-carbon-emacs-")
-(defvar el-init:cocoa-emacs-regexp  "^init-cocoa-emacs-")
-(defvar el-init:nw-regexp           "^init-nw-")
+;; regexps
+(defvar el-init:meadow-regexp "^init-meadow-"
+  "A regexp which indicates configuration files for meadow.
+This is used by `el-init:require/system-case'.")
 
-(defun el-init:meadowp        () (featurep 'meadow))
-(defun el-init:carbon-emacs-p () (featurep 'carbon-emacs-package))
-(defun el-init:cocoa-emacs-p  () (eq window-system 'ns))
-(defun el-init:nwp            () (null window-system))
+(defvar el-init:carbon-emacs-regexp "^init-carbon-emacs-"
+  "A regexp which indicates configuration files for carbon emacs.
+This is used by `el-init:require/system-case'.")
 
-(defvar el-init:mac-regexp     "^init-mac-")
-(defvar el-init:windows-regexp "^init-windows-")
-(defvar el-init:linux-regexp   "^init-linux-")
-(defvar el-init:freebsd-regexp "^init-freebsd-")
+(defvar el-init:cocoa-emacs-regexp "^init-cocoa-emacs-"
+  "A regexp which indicates configuration files for cocoa emacs.
+This is used by `el-init:require/system-case'.")
 
-(defun el-init:macp     () (string-match-p "apple-darwin" system-configuration))
-(defun el-init:windowsp () (string-match-p "mingw" system-configuration))
-(defun el-init:linuxp   () (string-match-p "linux" system-configuration))
-(defun el-init:freebsdp () (string-match-p "freebsd" system-configuration))
+(defvar el-init:nw-regexp "^init-nw-"
+  "A regexp which indicates configuration files for \"emacs -nw\".
+This is used by `el-init:require/system-case'.")
+
+(defvar el-init:mac-regexp "^init-mac-"
+  "A regexp which indicates configuration files for emacs on mac.
+This is used by `el-init:require/system-case'.")
+
+(defvar el-init:windows-regexp "^init-windows-"
+  "A regexp which indicates configuration files for emacs on windows.
+This is used by `el-init:require/system-case'.")
+
+(defvar el-init:linux-regexp "^init-linux-"
+  "A regexp which indicates configuration files for emacs on linux.
+This is used by `el-init:require/system-case'.")
+
+(defvar el-init:freebsd-regexp "^init-freebsd-"
+  "A regexp which indicates configuration files for emacs on freebsd.
+This is used by `el-init:require/system-case'.")
+
+;; predicates
+
+(defun el-init:meadowp ()
+  "Return non-nil if the running emacs is meadow.
+This is used by `el-init:require/system-case'."
+  (featurep 'meadow))
+
+(defun el-init:carbon-emacs-p ()
+  "Return non-nil if the running emacs is carbon emacs.
+This is used by `el-init:require/system-case'."
+  (featurep 'carbon-emacs-package))
+
+(defun el-init:cocoa-emacs-p ()
+  "Return non-nil if the running emacs is cocoa emacs.
+This is used by `el-init:require/system-case'."
+  (eq window-system 'ns))
+
+(defun el-init:nwp ()
+  "Return non-nil if the running emacs is \"emacs -nw\".
+This is used by `el-init:require/system-case'."
+  (null window-system))
+
+(defun el-init:macp ()
+  "Return non-nil if emacs is running on mac.
+This is used by `el-init:require/system-case'."
+  (string-match-p "apple-darwin" system-configuration))
+
+(defun el-init:windowsp ()
+  "Return non-nil if emacs is running on windows.
+This is used by `el-init:require/system-case'."
+  (string-match-p "mingw" system-configuration))
+
+(defun el-init:linuxp ()
+  "Return non-nil if emacs is running on linux.
+This is used by `el-init:require/system-case'."
+  (string-match-p "linux" system-configuration))
+
+(defun el-init:freebsdp ()
+  "Return non-nil if emacs is running on freebsd.
+This is used by `el-init:require/system-case'."
+  (string-match-p "freebsd" system-configuration))
 
 (defvar el-init:system-case-alist
   (list (cons el-init:meadow-regexp       #'el-init:meadowp)
@@ -193,9 +301,21 @@
         (cons el-init:mac-regexp          #'el-init:macp)
         (cons el-init:windows-regexp      #'el-init:windowsp)
         (cons el-init:linux-regexp        #'el-init:linuxp)
-        (cons el-init:freebsd-regexp      #'el-init:freebsdp)))
+        (cons el-init:freebsd-regexp      #'el-init:freebsdp))
+  "A list of \"(regexp . predicate)\".
+The regexp indicates whether a configuration file is for a system.
+The predicate is a function; it returns non-nil if the execution environment
+matches a system.
+This is used by `el-init:require/system-case'.")
 
 (defun el-init:require/system-case (feature &optional filename noerror)
+  "A `require' wrapper function to switch configuration files to load.
+The switching is based on `el-init:system-case-alist'.
+If a regexp of `el-init:system-case-alist' matches a feature name of
+a configuration file and the paired predicate function returns non-nil,
+the file will be loaded, but it will not if the predicate returns nil.
+If all the regexps don't match a feature name, the file will also be loaded.
+This wrapper records no values."
   (let ((match (cl-loop for (regexp . predicate) in el-init:system-case-alist
                         when (string-match-p regexp (symbol-name feature))
                         return predicate)))
@@ -228,12 +348,27 @@
        (el-init::file-name-el it)))))
 
 (defun el-init:require/record-old-library (feature &optional filename noerror)
+  "A `require' wrapper function to record whether a configuration file has an old .elc file.
+The record value is boolean; if old .elc file exists, the value is t.
+
+Example:
+  (el-init:get-record 'init-foo 'el-init:require/record-old-library)
+  ;; => t"
   (el-init:add-record feature
                       'el-init:require/record-old-library
                       (and (el-init::old-library-p (or filename feature)) t))
   (el-init:next feature filename noerror))
 
 (defun el-init:require/compile-old-library (feature &optional filename noerror)
+  "A `require' wrapper function to compile an old .elc file of a configuration file.
+The compilation is executed before loading the configuration file.
+The record value is boolean; non-nil means compilation is successful and
+nil means failure.
+If the .elc file is not old, this wrapper records no values.
+
+Example:
+  (el-init:get-record 'init-foo 'el-init:require/record-old-library)
+  ;; => t"
   (when (el-init::old-library-p (or filename feature))
     (let ((result (el-init::byte-compile-library (or filename feature))))
       (el-init:add-record feature
@@ -244,9 +379,17 @@
 
 ;; lazy loading
 
-(defvar el-init:lazy-init-regexp "^init-lazy-\\(.+\\)$")
+(defvar el-init:lazy-init-regexp "^init-lazy-\\(.+\\)$"
+  "A regexp which matches configuration files for lazy loading.
+The first group of the regexp indicates a feature name as an argument
+of `eval-after-load'.
+This is used by `el-init:require/lazy'.")
 
 (defun el-init:require/lazy (feature &optional filename noerror)
+  "A `require' wrapper function for lazy loading.
+If a configuration file feature name matches `el-init:lazy-init-regexp',
+this calls `eval-after-load' instead of loading it.
+This wrapper records no values."
   (save-match-data
     (if (string-match el-init:lazy-init-regexp (symbol-name feature))
         (eval-after-load (match-string 1 (symbol-name feature))
@@ -258,12 +401,31 @@
 
 ;;;; Loader
 
-(defvar el-init:load-file-regexp "\\.elc?$")
-(defvar el-init:subdirectories '("."))
-(defvar el-init:override-only-init-files-p t)
-(defvar el-init:overridep t)
-(defvar el-init:before-load-hook nil)
-(defvar el-init:after-load-hook nil)
+(defvar el-init:load-file-regexp "\\.elc?$"
+  "A regexp which matches configuration file names.")
+
+(defvar el-init:subdirectories '(".")
+  "A default value of the subdirectories parameter of `el-init:load'.")
+
+(defvar el-init:wrappers '(el-init:require/record-error)
+  "A default value of the wrappers parameter of `el-init:load'.")
+
+(defvar el-init:override-only-init-files-p t
+  "A default value of the override-only-init-files parameter of `el-init:load'.")
+
+(defvar el-init:overridep t
+  "A default value of the override parameter of `el-init:load'.")
+
+(defvar el-init:before-load-hook nil
+  "A hook which is run before loading of `el-init:load'.")
+
+(defvar el-init:after-load-hook nil
+  "A hook which is run after loading of `el-init:load'.")
+
+(define-obsolete-variable-alias
+  'el-init:load-function-list
+  'el-init:wrappers
+  "0.1.0")
 
 (define-obsolete-variable-alias
   'el-init:load-directory-list
@@ -321,6 +483,20 @@
                         ;; for compatibility with 0.0.9
                         (directory-list nil directory-list-flag)
                         (function-list nil function-list-flag))
+  "Load configuration files in DIRECTORY with `require'.
+
+DIRECTORY is a path of a directory which is root of configuration files.
+SUBDIRECTORIES is a list of subdirectories of DIRECTORY; its element is
+a string or a list like (\"path\" t).
+ means including all the directories in \"path\".
+OVERRIDE-ONLY-INIT-FILES is a flag to use overridden `require' only for
+configuration files.
+OVERRIDE is a flag to use overridden `require' when `require' called in
+configuration files.
+
+The mechanism:
+- Add SUBDIRECTORIES to `load-path'
+- Call `require' for all the configure files in SUBDIRECTORIES"
   ;; for compatibility with 0.0.9
   (when directory-list-flag
     (setq subdirectories directory-list)

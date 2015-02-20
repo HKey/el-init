e76344c876cfd31795960e72081226972634a16f
HKey <s06139@gmail.com>
2015-02-20 14:33:17 +0900

    Add URL header

--- el-init.el
+++ el-init.el
@@ -3,6 +3,7 @@
 ;; Copyright (C) 2015  Hiroki YAMAKAWA
 
 ;; Author: Hiroki YAMAKAWA <s06139@gmail.com>
+;; URL: https://github.com/HKey/el-init
 ;; Version: 0.1.4
 ;; Package-Requires: ((emacs "24") (cl-lib "0.5") (anaphora "1.0.0"))
 ;; Keywords:
